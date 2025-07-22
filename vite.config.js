b4d16cbf928149effc780c817d367b38c8bbaa20
Rohit Kumar Saxena <122184390+rohit-saxena-onGit@users.noreply.github.com>
2025-07-22 12:11:39 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,17 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import tailwindcss from '@tailwindcss/vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [tailwindcss(),react()],
+//   base: './',
+// })
+
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
-  plugins: [tailwindcss(),react()],
-  base: './',
-})
+  plugins: [react()],
+});
